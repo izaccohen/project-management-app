020987d4f1e8dc3fa48cf2e0c477de749121e45e
izac cohen <izaccohen30@gmail.com>
2018-12-03 12:38:20 +0200

    routing

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-var app = angular.module("projMngApp", []);
+var app = angular.module("projMngApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "app/home/home.html"
+    }).when("/login", {
+        templateUrl: "app/login/login.html",
+        controller: "loginCtrl"
+
+    }).when("/division" , {
+        templateUrl: "app/division/divisionhome.html",
+        controller: "newRecipeCtrl"
+    })
+
+    .when("/crew", {
+        templateUrl: "app/crew/crewhome.html",
+        controller: "recipeGalleryCtrl"
+    })
+})
 
 
