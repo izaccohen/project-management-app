520526299e71267be56aa2a43b295838e172a8e8
izaccohen <izaccohen30@gmail.com>
2018-12-16 18:54:44 +0200

    https for login

--- app/model/userSrv.js
+++ app/model/userSrv.js
@@ -17,7 +17,7 @@ app.factory("user", function($q, $http,tasks) {
 
         var loginURL = "http://my-json-server.typicode.com/izaccohen/project-management-app/users?email=" +
             email + "&pwd=" + pwd;
-        $http.get(loginURL).then(function(response) {
+        $https.get(loginURL).then(function(response) {
             if (response.data.length > 0) {
                 // success login
                 activeUser = new User(response.data[0]);
